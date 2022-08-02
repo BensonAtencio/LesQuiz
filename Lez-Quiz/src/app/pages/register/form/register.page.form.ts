4a456fed0c86dff36ef717f44236a1ad3f5c32dd
Lance Carenan <109298191+LanceGC@users.noreply.github.com>
2022-08-02 16:18:06 +0800

    removing some comments

--- Lez-Quiz/src/app/pages/register/form/register.page.form.ts
+++ Lez-Quiz/src/app/pages/register/form/register.page.form.ts
@@ -3,11 +3,9 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterPageForm {
 
     private formBuilder: FormBuilder;
-    //private form: FormGroup;
 
     constructor(formBuilder: FormBuilder){
         this.formBuilder = formBuilder;
-       // this.form = this.createForm();
     }
 
     createForm(): FormGroup {
@@ -16,10 +14,7 @@ export class RegisterPageForm {
             email: ['', [Validators.required, Validators.email]],
             password: ['', [Validators.required, Validators.minLength(6)]]
         });
-        //return form;
     }
 
-    /*getForm() : FormGroup{
-        return this.form;
-    }*/
+
 }
